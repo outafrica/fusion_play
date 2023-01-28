746c3020c18a12beb39c690eb1aed526dd9ce890
outafrica <george.jarviz@gmail.com>
2023-01-28 13:58:03 +0300

    Fixes Playlist Storage

--- database/migrations/2023_01_27_180743_create_user_playlists_table.php
+++ database/migrations/2023_01_27_180743_create_user_playlists_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('user_playlists', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->string('playlists');
+            $table->text('playlists');
             $table->timestamps();
         });
     }
